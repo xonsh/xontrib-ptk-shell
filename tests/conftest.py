61091633382847c59ac1ada9fb03119ab32429f2
Noortheen Raja <jnoortheen@gmail.com>
2022-07-05 12:10:34 +0530

    feat: add coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,14 @@ import pytest
 
 
 @pytest.fixture
-def ptk_xontrib(xession):
+def xsh(xonsh_session, env, monkeypatch):
+    # here we need XSH.exit and not mocking any
+    monkeypatch.setattr(xonsh_session, "env", env)
+    return xonsh_session
+
+
+@pytest.fixture
+def ptk_xontrib(xsh):
     from xonsh.xontribs import xontribs_load, xontribs_unload
 
     mod = "xontrib_ptk_shell.main"
@@ -14,7 +21,7 @@ def ptk_xontrib(xession):
 
 
 @pytest.fixture
-def ptk_shell(ptk_xontrib, xession):
+def ptk_shell(ptk_xontrib, xsh):
     from prompt_toolkit.input import create_pipe_input
     from prompt_toolkit.output import DummyOutput
 
@@ -23,6 +30,6 @@ def ptk_shell(ptk_xontrib, xession):
     out = DummyOutput()
     with create_pipe_input() as inp:
         shell = PromptToolkitShell(
-            execer=xession.execer, ctx={}, ptk_args={"input": inp, "output": out}
+            execer=xsh.execer, ctx={}, ptk_args={"input": inp, "output": out}
         )
         yield inp, out, shell
